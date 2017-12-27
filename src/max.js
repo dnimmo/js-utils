baf1987e442e953b95c2b1dbf6e9fbd58bd2319d
David Nimmo <dnimmo@gmail.com>
2017-12-27 11:23:45 +0000

    add strings to max

--- src/max.js
+++ src/max.js
@@ -1,4 +1,11 @@
+const getHighest =
+  (x, y) => Math.max(parseInt(x), parseInt(y));
+
 const max =
-  arr => arr.reduce((x, y) => Math.max(x, y));
+  (...arr) => 
+    typeof arr[0] === 'object' 
+    && arr[0].length 
+      ? arr[0].reduce(getHighest)
+      : arr.reduce(getHighest);
 
 export default max;
--- test/max.test.js
+++ test/max.test.js
@@ -7,4 +7,18 @@ describe('max', () => {
       expect(max([100, 1, 2]))
         .toBe(100)
   );
+
+  it(
+    'should find the highest value of all of the numbers passed in',
+    () =>
+      expect(max(100, 1, 2))
+        .toBe(100)
+  );
+
+  it(
+    'should find the highest numeric value even if it is a string',
+    () =>
+      expect(max('100', 1, 2))
+        .toBe(100)
+  )
 });
